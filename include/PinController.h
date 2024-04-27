84a252822344babb67253fd3e55248e6e3b32903
Joe Rybka <6586029+joerybka@users.noreply.github.com>
2024-04-26 20:02:34 -0400

    document pin controller and rename some vars

--- include/PinController.h
+++ include/PinController.h
@@ -7,67 +7,77 @@
 #define THERMOSTATIO_PINCONTROLLER_H
 
 enum IoMode {
-    In = 0,
-    Out = 1,
+  In = 0,
+  Out = 1,
 };
 
 class PinController {
 private:
-    uint8_t _pin;
-    uint8_t _mode;
-    bool _inverted = false;
+  /// The pin number that this controller represents
+  uint8_t _pin;
 
-    IoMode _ioMode;
+  /// The mode of the pin to set during initialization
+  uint8_t _mode;
 
-    bool _setOn = false;
+  /// Whether the logic of this pin is inverted, set this if you want "on" to represent LOW and "off" to represent HIGH
+  bool _inverted = false;
 
-    uint8_t _onValue() {
-        return _inverted ? LOW : HIGH;
-    }
+  /// A higher-level in/out enum so that we can not care about comparing with mode
+  IoMode _ioMode;
 
-    uint8_t _offValue() {
-        return _inverted ? HIGH : LOW;
-    }
+  /// If this is an "Out" controller, this will hold the last value we wrote to the pin
+  bool _setOn = false;
+
+  /// The value to use in this controller to represent "On"
+  uint8_t _onValue() const {
+    return _inverted ? LOW : HIGH;
+  }
+
+  /// The value to use in this controller to represent "Off"
+  uint8_t _offValue() const {
+    return _inverted ? HIGH : LOW;
+  }
+
 public:
-    /**
-     * Initialize the pin controller, this is a light wrapper around the digital functions for arduino.
-     * @param pin The arduino pin number to control
-     * @param mode The pin mode
-     */
-    PinController(uint8_t pin, uint8_t mode);
+  /**
+   * Initialize the pin controller, this is a light wrapper around the digital functions for arduino.
+   * @param pin The arduino pin number to control
+   * @param mode The pin mode
+   */
+  PinController(uint8_t pin, uint8_t mode);
 
-    /**
-     * You must call initialize to run the pin setup.  Output pins will be set to this controller's Off value.
-     */
-    void Initialize();
+  /**
+   * You must call initialize to run the pin setup.  Output pins will be set to this controller's Off value.
+   */
+  void Initialize();
 
-    /**
-     * Flip the current inversion setting
-     * @return The new inversion setting
-     */
-    bool Invert();
+  /**
+   * Flip the current inversion setting
+   * @return The new inversion setting
+   */
+  bool Invert();
 
-    /**
-     * Checks if the pin set to high in write mode or if it is high in read mode
-     * @return True if the pin reads on, or if the pin state is set to high in write mode
-     */
-    bool IsOn();
+  /**
+   * Checks if the pin set to high in write mode or if it is high in read mode
+   * @return True if the pin reads on, or if the pin state is set to high in write mode
+   */
+  bool IsOn();
 
-    /**
-     * Checks if the pin is set to low in write mode or if it is low in read mode
-     * @return True if the pin reads off, or if the pin state is set to low in write mode
-     */
-    bool IsOff();
+  /**
+   * Checks if the pin is set to low in write mode or if it is low in read mode
+   * @return True if the pin reads off, or if the pin state is set to low in write mode
+   */
+  bool IsOff();
 
-    /**
-     * Sets the pin to high in write mode, otherwise no action
-     */
-    void SetPinOn();
+  /**
+   * Sets the pin to high in write mode, otherwise no action
+   */
+  void SetPinOn();
 
-    /**
-     * Sets the pin to low in write mode, otherwise no action
-     */
-    void SetPinOff();
+  /**
+   * Sets the pin to low in write mode, otherwise no action
+   */
+  void SetPinOff();
 };
 
 #endif //THERMOSTATIO_PINCONTROLLER_H
--- src/PinController.cpp
+++ src/PinController.cpp
@@ -6,37 +6,39 @@
 #include "PinController.h"
 
 PinController::PinController(uint8_t pin, uint8_t mode) : _pin(pin), _mode(mode) {
-    // internals of Arduino default to write if the mode is bad
-    if(_mode == INPUT || _mode == INPUT_PULLUP) _ioMode = In;
-    else _ioMode = Out;
+  // internals of Arduino default to write if the mode is bad
+  if (_mode == INPUT || _mode == INPUT_PULLUP) _ioMode = In;
+  else _ioMode = Out;
 }
 
 void PinController::Initialize() {
-    pinMode(_pin, _mode);
-    if (_ioMode == Out) digitalWrite(_pin, _offValue());
+  pinMode(_pin, _mode);
+  SetPinOff();  // ensure predictable start values
 }
 
 bool PinController::Invert() {
-    _inverted = !_inverted;
-    return _inverted;
+  _inverted = !_inverted;
+  return _inverted;
 }
 
 bool PinController::IsOn() {
-    return _ioMode == In ? digitalRead(_pin) == _onValue() : _setOn;
+  return _ioMode == In ? digitalRead(_pin) == _onValue() : _setOn;
 }
 
 bool PinController::IsOff() {
-    return _ioMode == In ? digitalRead(_pin) == _offValue() : !_setOn;
+  return _ioMode == In ? digitalRead(_pin) == _offValue() : !_setOn;
 }
 
 void PinController::SetPinOn() {
-    if(_ioMode == In) return;
+  if (_ioMode == In) return;
 
-    digitalWrite(_pin, _onValue());
+  _setOn = true;
+  digitalWrite(_pin, _onValue());
 }
 
 void PinController::SetPinOff() {
-    if(_ioMode == In) return;
+  if (_ioMode == In) return;
 
-    digitalWrite(_pin, _offValue());
+  _setOn = false;
+  digitalWrite(_pin, _offValue());
 }
